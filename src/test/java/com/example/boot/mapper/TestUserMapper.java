44f3642f8273b07c7225b633a947fa5098efb7a0
Scoheart <Scoheart1487790559@gmail.com>
2023-06-27 22:52:20 +0800

    feat: add new code

--- src/test/java/com/example/boot/mapper/TestUserMapper.java
+++ src/test/java/com/example/boot/mapper/TestUserMapper.java
@@ -32,4 +32,11 @@ public class TestUserMapper {
         System.out.println(insert);
     }
 
+    @Test
+    void test() {
+        User user = new User();
+        user.setUsername("s1111111");
+        User user1 = userMapper.selectById(1);
+    }
+
 }

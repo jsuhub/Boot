1c17c5e2f823d470ff906500c636a2358569c10c
Scoheart <Scoheart1487790559@gmail.com>
2023-07-01 18:38:35 +0800

    feat: add new code

--- src/main/java/com/example/boot/controller/QuestionController.java
+++ src/main/java/com/example/boot/controller/QuestionController.java
@@ -1,11 +1,15 @@
 package com.example.boot.controller;
+
 import com.example.boot.constant.Status;
+import com.example.boot.constant.StatusInfo;
 import com.example.boot.pojo.entity.Article;
 import com.example.boot.pojo.entity.Question;
+import com.example.boot.pojo.vo.RequestVO;
 import com.example.boot.pojo.vo.ResponseVO;
 import com.example.boot.service.impl.QuestionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 import java.util.List;
@@ -20,88 +24,95 @@ public class QuestionController {
 
     /**
      * 新增一个问题
-     * @param questionResponseVO 请求数据实体
-     * @return ResponseVO<Boolean> 响应数据实体
+     *
+     * @param questionRequestVO 问题的请求参数
+     * @return Boolean 布尔值 是否插入成功
      */
     @PostMapping
-    ResponseVO<Boolean> saveQuestion(@RequestBody ResponseVO<Question> questionResponseVO){
-        boolean save=questionService.save(questionResponseVO.getData());
-        return save
-                ? new ResponseVO<Boolean>(Status.SUCCESS,"save ok",save)
-                : new ResponseVO<Boolean>(Status.ERROR,"save error",save);
+    ResponseVO<Boolean> saveQuestion(@RequestBody RequestVO<Question> questionRequestVO) {
+        boolean saveQuestion = questionService.save(questionRequestVO.getData());
+        return saveQuestion
+                ? new ResponseVO<>(Status.SUCCESS, StatusInfo.SAVE_INFO_SUCCESS, saveQuestion)
+                : new ResponseVO<>(Status.ERROR, StatusInfo.SAVE_INFO_FAILED, saveQuestion);
     }
 
     /**
-     * 根据问题的id删除这个问题
-     * @param id 问题的唯一标识
-     * @return ResponseVO<Boolean> 响应数据实体
+     * 根据id删除问题
+     *
+     * @param id 问题id
+     * @return Boolean 布尔值 是否删除成功
      */
     @DeleteMapping("/{id}")
-    ResponseVO<Boolean>removeQuestion(@PathVariable int id){
-        boolean remove=questionService.removeById(id);
+    ResponseVO<Boolean> removeQuestion(@PathVariable int id) {
+        boolean remove = questionService.removeById(id);
         return remove
-                ?new ResponseVO<Boolean>(Status.SUCCESS,"remove ok",remove)
-                :new ResponseVO<>(Status.ERROR,"remove error",remove);
+                ? new ResponseVO<Boolean>(Status.SUCCESS, "remove ok", remove)
+                : new ResponseVO<>(Status.ERROR, "remove error", remove);
     }
 
     /**
-     * 对一个问题的内容进行更改
+     * 修改问题
+     *
      * @param questionResponseVO 请求数据实体
      * @return ResponseVO<Boolean> 响应数据实体
      */
     @PutMapping
-    ResponseVO<Boolean> updateQuestion(@RequestBody ResponseVO<Question> questionResponseVO ){
-        boolean update=questionService.updateById(questionResponseVO.getData());
+    ResponseVO<Boolean> updateQuestion(@RequestBody ResponseVO<Question> questionResponseVO) {
+        boolean update = questionService.updateById(questionResponseVO.getData());
         return update
-                ?new ResponseVO<Boolean>(Status.SUCCESS,"update ok",update)
-                :new ResponseVO<Boolean>(Status.ERROR,"update error",update);
+                ? new ResponseVO<Boolean>(Status.SUCCESS, "update ok", update)
+                : new ResponseVO<Boolean>(Status.ERROR, "update error", update);
     }
 
     /**
      * 根据id查询一个问题
+     *
      * @param id 问题的唯一标识
      * @return ResponseVO<Question> 响应数据实体
      */
     @GetMapping("/{id}")
-    ResponseVO<Question>getQuestion(@PathVariable  int id){
+    ResponseVO<Question> getQuestion(@PathVariable int id) {
         Question byId = questionService.getById(id);
-        return byId !=null
-                ?new ResponseVO<>(Status.SUCCESS,"get a question",byId)
-                :new ResponseVO<>(Status.ERROR,"get failed",byId);
+        return byId != null
+                ? new ResponseVO<>(Status.SUCCESS, "get a question", byId)
+                : new ResponseVO<>(Status.ERROR, "get failed", byId);
     }
 
     /**
      * 根据问题id计算该问题的权重(热度)
+     *
      * @param id 问题的唯一标识
      * @return ResponseVO<Boolean> 响应数据实体
      */
     @GetMapping("/compute/{id}")
-    ResponseVO<Boolean> computeWeighRatio(@PathVariable int id){
+    ResponseVO<Boolean> computeWeighRatio(@PathVariable int id) {
         Boolean aBoolean = questionService.computeWeighRatio(id);
         return aBoolean
-                ?new ResponseVO<>(Status.SUCCESS,"compute successfully",aBoolean)
-                :new ResponseVO<>(Status.ERROR,"compute weigh Ratio fail",aBoolean);
+                ? new ResponseVO<>(Status.SUCCESS, "compute successfully", aBoolean)
+                : new ResponseVO<>(Status.ERROR, "compute weigh Ratio fail", aBoolean);
     }
 
     /**
      * 根据时间降序返回当前库中所有的问题
-     * @return ResponseVO<List<Question>> 响应数据实体
+     *
+     * @return ResponseVO<List < Question>> 响应数据实体
      */
     @GetMapping("/timeQuestion")
-    ResponseVO<List<Question>> returnQuestionByTimeAsc(){
+    ResponseVO<List<Question>> returnQuestionByTimeAsc() {
         List<Question> question = questionService.returnQuestionByTimeAsc();
-        return question!=null
-                ?new ResponseVO<>(Status.SUCCESS,"Asc successfully",question)
-                :new ResponseVO<>(Status.SUCCESS,"Asc successfully",question);
+        return question != null
+                ? new ResponseVO<>(Status.SUCCESS, "Asc successfully", question)
+                : new ResponseVO<>(Status.SUCCESS, "Asc successfully", question);
     }
 
     /**
      * 根据当天发布的问题的权重返回降序后的所有问题
+     *
      * @param time 当天的时间
-     * @return ResponseVO<List<Question>> 响应数据实体
+     * @return ResponseVO<List < Question>> 响应数据实体
      */
     @GetMapping("/hotQuestion/{time}")
-    ResponseVO<List<Question>> returnQuestionToWebByWeighRatio(@PathVariable String time){
+    ResponseVO<List<Question>> returnQuestionToWebByWeighRatio(@PathVariable String time) {
         List<Question> questions = questionService.returnQuestionToWebByWeighRatio(questionService.timeFormat(time));
         return questions != null
                 ? new ResponseVO<>(Status.SUCCESS, "desc successfully", questions)
